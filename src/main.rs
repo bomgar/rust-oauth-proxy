9ab4401c66e674a747e1e00e6f7767409371cd49
Patrick Haun <bomgar85@googlemail.com>
2016-10-10 08:34:36 +0200

    add better error handling

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,46 @@ use slog::DrainExt;
 use slog::Logger;
 use clap::{Arg, App, AppSettings};
 use hyper::server::{Server, Request, Response};
-use hyper::client::{Client};
+use hyper::client::Client;
 use std::net::ToSocketAddrs;
+
+use std::error::Error;
+use std::fmt;
+use std::convert::From;
+
+#[derive(Debug)]
+struct ProxyError {
+  message: String,
+}
+
+impl fmt::Display for ProxyError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}", &self.message)
+  }
+}
+
+impl Error for ProxyError {
+  fn description(&self) -> &str {
+    &self.message
+  }
+}
+
+impl From<hyper::Error> for ProxyError {
+  fn from(e: hyper::Error) -> ProxyError{
+    ProxyError {
+      message: format!("{}", e)
+    }
+  }
+}
+
+impl From<std::io::Error> for ProxyError {
+  fn from(e: std::io::Error) -> ProxyError{
+    ProxyError {
+      message: format!("{}", e)
+    }
+  }
+}
+
 
 fn main() {
   let matches = App::new("rust oauth proxy")
@@ -46,7 +84,9 @@ fn main() {
     info!(log, "Server started."; "bind_address" => bind_address.to_string());
     server.handle(move |request: Request, response: Response| {
         let log = log.new(o!("correlation_id" => create_correlation_id()));
-        proxy_request(log, request, response);
+        if let Err(e) = proxy_request(&log, request, response) {
+          error!(log, "{}", e)
+        }
       })
       .unwrap();
   } else {
@@ -54,19 +94,20 @@ fn main() {
   }
 }
 
-fn proxy_request(log: Logger, request: Request, mut response: Response) {
+fn proxy_request(log: &Logger, request: Request, mut response: Response) -> Result<(), ProxyError> {
   info!(log, "Incoming request";
               "from" => request.remote_addr.to_string(),
               "method" => request.method.to_string(),
               "uri" => request.uri.to_string()
       );
   let client = Client::new();
-  let mut proxy_response = client.get("http://google.com/").send().unwrap();
+  let mut proxy_response = try!(client.get("http://google.com/").send());
   *response.status_mut() = proxy_response.status.clone();
   *response.headers_mut() = proxy_response.headers.clone();
-  let mut response = response.start().unwrap();
-  std::io::copy(&mut proxy_response, &mut response).unwrap();
-  response.end().unwrap();
+  let mut response = try!(response.start());
+  try!(std::io::copy(&mut proxy_response, &mut response));
+  try!(response.end());
+  Ok(())
 }
 
 fn create_correlation_id() -> String {
